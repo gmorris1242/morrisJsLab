7550eef9cc4fe4a2be36b78d21647f4c20a651e0
George Morris <gmorris1242@gmail.com>
2018-01-30 16:40:05 -0500

    add objects

--- script.js
+++ script.js
@@ -1,72 +1,102 @@
 // try a var keeping track of the amount of times grant hits zero in order to track the
 // round and be able to say something differnet after each round
 
-var userName= ""
-var userHealth = 40;
-var grantHealth = 10;
-var userWins = 0;
-var grantWins = 0;
+(function () {
 
+var character = {
+  name: "",
+  health: 40,
+  wins: 0,
+  healsRemaining: 2,
+  generateAttackDamage:  function() {
+     return Math.floor(Math.random() * 3) + 1;
+   },
+  heal: function() {
+      this.health += Math.floor(Math.random() * 10) + 1;
+   },
+}
+
+var grant = {
+  name: "Grant",
+  health: 10,
+  wins: 0,
+  generateAttackDamage:  function() {
+     return Math.floor(Math.random() * 5) + 1;
+   },
+}
 
 function startGame() {
   var question = prompt("Do you want to play?");
   if (question.toLowerCase() === "yes") {
-    userName = prompt("What is your name?");
+    character.name = prompt("What is your name?");
     startCombat();
   } else {
     console.log("You have left the game")
   }
 }
 
-
- function getDamage() {
-   return Math.floor(Math.random() * 5) + 1;
- }
-                                //console.log(getDamage()); random number test.
-
 function startCombat() {
-  while (userWins < 3 && grantWins <  1) {
 
-    userHealth-= getDamage();
-    grantHealth-= getDamage();
-    console.log(userName + " has " + userHealth + " left.");
-    console.log("Grant the Mighty Chicken has " + grantHealth + " left.");
-    if (grantHealth <= 0) {
-      userWins++;
-      grantHealth = 10
-      if (userWins === 1) {
-        console.log(userName + " wins round one!")
-      } else if (userWins === 2) {
-        console.log(userName + " wins round two!  One more to go!")
-      }
-    } else if (userHealth <= 0) {
-      grantWins++;
+  while (character.wins < 5 && grant.wins <  1) {
+
+
+    if ( character.healsRemaining > 0){
+    var attackOrQuit = prompt("Attack, Heal or Quit?");
+    attackOrQuit = attackOrQuit.toLowerCase();
+  } else {attackOrQuit = prompt("Attack or Quit?");
+          attackOrQuit = attackOrQuit.toLowerCase();}
+
+
+    while (attackOrQuit !== "attack" && attackOrQuit !== "quit" && attackOrQuit !== "heal"){
+       attackOrQuit = prompt("Choose Attack, Heal or Quit?");
     }
+    if (attackOrQuit === "quit") {
+        console.log("You have left the game.")
+        break;
+    } else if (attackOrQuit === "heal"  && character.healsRemaining > 0) {
+        character.heal();
+        character.healsRemaining--;
+        console.log(character.name + " has healed and has " + character.health + " and has " + character.healsRemaining + " heal remaing." );
+    } else if(attackOrQuit === "heal"  && character.healsRemaining === 0){
+      console.log("You don't have any heals left");
+    } else if ( attackOrQuit === "attack"){
 
 
+    character.health-= grant.generateAttackDamage();
+    grant.health-= character.generateAttackDamage();
+    console.log(character.name + " has " + character.health+ " left.");
+    console.log("Grant the Mighty Chicken has " + grant.health + " left.");
 
-  if (userWins === 3) {
-    console.log(userName + " has defeated Grant!");
-  } else if (grantWins > 0) {
-    console.log("Grant Wins!");
-  } else if (userHealth === 0 && grantHealth === 0){
-    console.log("It's a tie!")
-  // }else {
-  //   console.log("You have left the game")
-  }
+    if (grant.health <= 0) {
+      character.wins++;
+      grant.health = 10
+      if (character.wins === 1) {
+        console.log(character.name + " wins round one!")
+      } else if (character.wins === 2) {
+        console.log(character.name + " wins round two!")
+      } else if (character.wins === 3) {
+        console.log(character.name + " wins round three!")
+      } else if (character.wins === 4) {
+        console.log(character.name + " wins round four!  One more to go!")
+      }
+      } else if (character.health <= 0) {
+      grant.wins++;
+      }
 
-  if (userWins < 3 && grantWins <  1){
-    var attackOrQuit = prompt("Attack or Quit?");
-  attackOrQuit = attackOrQuit.toLowerCase();
-  while (attackOrQuit !== "attack" && attackOrQuit !== "quit"){
-     attackOrQuit = prompt("Choose Attack or Quit");
-  }
-  if (attackOrQuit === "quit") {
-    console.log("You have left the game.")
-    break;
+    if (character.wins === 5) {
+      console.log(character.name + " has defeated Grant!");
+    } else if (grant.wins > 0) {
+      console.log("Grant Wins!");
+    } else if (character.health === 0 && grant.health === 0){
+      console.log("It's a tie!")
   }
 }
+
   }//loop
 }//function
 
 startGame();
+
+
+
+})();
